0d410d71640d8ea1887924be39b36d3ca19a0e01
wu-wilson <wilson.cui.wu@gmail.com>
2026-02-09 20:32:19 -0600

    chore - migrate vite react-ts

---
+++ src/App.tsx
@@ -0,0 +1,5 @@
+function App() {
+  return <div>Under Maintenance</div>;
+}
+
+export default App;
---
+++ vite.config.ts
@@ -0,0 +1,14 @@
+import path from "path";
+import tailwindcss from "@tailwindcss/vite";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+export default defineConfig({
+  plugins: [react(), tailwindcss()],
+  base: "/",
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
