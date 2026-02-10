72585d1dabd022643bcbc971904290cd14f796b6
wu-wilson <wilson.cui.wu@gmail.com>
2026-02-09 20:43:18 -0600

    added theme provider context

---
+++ src/contexts/theme-provider/types.ts
@@ -0,0 +1,14 @@
+import type { ReactNode } from "react";
+
+export type Theme = "dark" | "light";
+
+export type ThemeProviderProps = {
+  children: ReactNode;
+  defaultTheme?: Theme;
+  storageKey?: string;
+};
+
+export type ThemeProviderState = {
+  theme: Theme;
+  setTheme: (theme: Theme) => void;
+};
