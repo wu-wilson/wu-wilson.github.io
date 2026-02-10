67ac999cc620587fe656f0cff4fde9cb564c36ac
wu-wilson <wilson.cui.wu@gmail.com>
2026-02-10 13:46:14 -0600

    added about me section

--- src/sections/hero.tsx
+++ src/sections/hero.tsx
@@ -1,39 +1,61 @@
 import { Button } from "@/components/ui/button";
-import { Github, Linkedin, Mail, LucideIcon } from "lucide-react";
+import { IconType } from "react-icons";
+import {
+  FiLinkedin,
+  FiGithub,
+  FiInstagram,
+  FiYoutube,
+  FiMail,
+  FiFileText,
+} from "react-icons/fi";
 
 type Social = {
   href: string;
   label: string;
-  icon: LucideIcon;
+  icon: IconType;
 };
 
 const socials: Social[] = [
   {
     href: "https://github.com/wu-wilson",
     label: "GitHub",
-    icon: Github,
+    icon: FiGithub,
   },
   {
     href: "https://linkedin.com/in/wils-wu",
     label: "LinkedIn",
-    icon: Linkedin,
+    icon: FiLinkedin,
+  },
+  {
+    href: "https://www.instagram.com/wilson_wu123",
+    label: "Instagram",
+    icon: FiInstagram,
+  },
+  {
+    href: "https://www.youtube.com/@wilsonwu6209",
+    label: "Youtube",
+    icon: FiYoutube,
   },
   {
     href: "mailto:wilson.cui.wu@gmail.com",
     label: "Email",
-    icon: Mail,
+    icon: FiMail,
   },
 ];
 
 function Hero() {
   return (
-    <div className="flex flex-col items-center w-full p-3 gap-3">
+    <div className="flex flex-col items-center w-full p-3 gap-5">
       <h1 className="text-5xl">Wilson Wu</h1>
       <h2 className="text-xl">Full-Stack Engineer</h2>
-      <span className="w-70 text-center mt-4">
+      <span className="w-70 text-center">
         I build apps that feel simple, clean, and easy to use.
       </span>
-      <div className="flex gap-3 mt-4">
+      <Button variant="outline">
+        <FiFileText />
+        Resume
+      </Button>
+      <div className="flex gap-3">
         {socials.map(({ href, label, icon: Icon }) => (
           <Button key={label} variant="outline" size="icon" asChild>
             <a
