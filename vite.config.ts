718f4df41de83dac59eb2bd5a77d9ffe000b632e
wu-wilson <wilson.cui.wu@gmail.com>
2026-02-09 20:34:18 -0600

    removed unnecessary config

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,6 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
