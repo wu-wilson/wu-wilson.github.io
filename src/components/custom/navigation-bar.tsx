aac122f78272814dffd00edce2e09f773923fe53
wu-wilson <wilson.cui.wu@gmail.com>
2026-02-11 12:12:16 -0600

    updated navbar blur

--- src/components/custom/navigation-bar.tsx
+++ src/components/custom/navigation-bar.tsx
@@ -2,7 +2,7 @@ import ThemeToggle from "./theme-toggle";
 
 function NavigationBar() {
   return (
-    <div className="sticky top-0 z-100 bg-background flex w-full justify-center border-b">
+    <div className="sticky top-0 z-100 flex w-full justify-center border-b backdrop-blur-lg">
       <div className="flex w-full max-w-7xl justify-end p-3">
         <ThemeToggle />
       </div>
